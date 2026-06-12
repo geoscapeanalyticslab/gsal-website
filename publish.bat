c7ae95f0ca301ee854731818e6c50a0cd521ad38
MUHAMMAD-MEHDI12 <geomehdi12@gmail.com>
2026-06-12 12:38:09 +0500

    Updates

---
+++ publish.bat
@@ -0,0 +1,10 @@
+@echo off
+cd /d "%~dp0"
+set /p msg="What did you change? "
+git add .
+git commit -m "%msg%"
+git push
+call npm run deploy
+echo.
+echo ====== DONE! Site will update in 1-2 minutes ======
+pause
